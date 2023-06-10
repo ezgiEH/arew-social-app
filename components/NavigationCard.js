0c73408d6b4371b982b1f5ae7968d4d81b13bab6
Ezgi Hocaoğlu <ezgihocaoglu@hotmail.com>
2023-06-10 17:43:43 +0300

    navigation

--- components/NavigationCard.js
+++ components/NavigationCard.js
@@ -1,14 +1,14 @@
 import { useRouter } from 'next/router'
 import Card from './Card'
 import Link from 'next/link'
-import { useSupabaseClient } from '@supabase/auth-helpers-react'
+import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
 
 export default function NavigationCard() {
     const router = useRouter()
     const { asPath: pathname } = router
     const activeElementClasses = 'text-sm md:text-md flex md:gap-1 md:gap-3 py-3 my-1 bg-socialBlue text-white md:-mx-8 md:px-8 px-6 rounded-md shadow-md shadow-gray-300'
     const nonActiveElementClasses = 'text-sm md:text-md flex md:gap-1 md:gap-3  py-2 my-2 hover:bg-blue-500 hover:bg-opacity-20 md:-mx-4 px-6 md:px-4 rounded-md transition-all hover:scale-110 hover:shadow-md shadow-gray-300'
-
+    const session =useSession()
 
     //logout function
     const supabase = useSupabaseClient()
@@ -26,7 +26,7 @@ export default function NavigationCard() {
                     </svg>
                     <span className='hidden md:block'>Home</span>
                 </Link>
-                <Link href="/profile/friends" className={pathname === '/profile/friends' ? activeElementClasses : nonActiveElementClasses}>
+                <Link href={`/profile/${session.user?.id}/friends`} className={pathname === '/profile/friends' ? activeElementClasses : nonActiveElementClasses}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z" />
                     </svg>
